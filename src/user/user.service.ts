11f586974590befa0883c40baaeb1df331b14cb1
HoussemAmmar <houssem.ammar.simplon@gmail.com>
2022-10-18 17:51:30 +0100

    add user schema

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AbstractService } from '../abstract/abstract.service';
+import { User } from './user.schema';
+import { Model } from 'mongoose';
+import { Logger } from 'winston';
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
-export class UserService {}
+export class UserService extends AbstractService<User> {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<User>,
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {
+    super(userModel, logger);
+  }
+  protected modelName = User.name;
+}
