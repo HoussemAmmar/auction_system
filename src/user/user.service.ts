ee49215bbdcb84e63233a996d7cc08639b6b0ca9
HoussemAmmar <houssem.ammar.simplon@gmail.com>
2022-10-19 12:46:05 +0100

    implement user module crud

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Body, Inject, Injectable } from '@nestjs/common';
 import { AbstractService } from '../abstract/abstract.service';
 import { User } from './user.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { Logger } from 'winston';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { InjectModel } from '@nestjs/mongoose';
+import { CreateUserDto } from './user.dto';
 
 @Injectable()
 export class UserService extends AbstractService<User> {
@@ -15,4 +16,12 @@ export class UserService extends AbstractService<User> {
     super(userModel, logger);
   }
   protected modelName = User.name;
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return this.create(createUserDto);
+  }
+
+  async deleteUser(user: Types.ObjectId): Promise<User> {
+    return this.findByIdAndDelete(new Types.ObjectId(user));
+  }
 }
