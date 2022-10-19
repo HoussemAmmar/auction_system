ee6b95b1b95e448db34ecb4cc02665da5eeedefe
houssem.ammar.06@gmail.com <houssem.ammar.06@gmail.com>
2022-10-19 16:29:46 +0100

    change all fields to required in user schema

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -2,20 +2,19 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { SchemaTypes } from 'mongoose';
 import { AbstractModel } from '../abstract/abstract.model';
 
-
 @Schema({
   timestamps: true,
 })
 export class User extends AbstractModel {
-  @Prop({ required: false, type: SchemaTypes.String })
+  @Prop({ required: true, type: SchemaTypes.String })
   firstName: string;
-  @Prop({ required: false, type: SchemaTypes.String })
+  @Prop({ required: true, type: SchemaTypes.String })
   lastName: string;
-  @Prop({ required: false, type: SchemaTypes.String })
-  address?: string;
-  @Prop({ required: false, type: SchemaTypes.String })
-  phoneNumber?: string;
-  @Prop({ required: false, type: SchemaTypes.Date })
+  @Prop({ required: true, type: SchemaTypes.String })
+  address: string;
+  @Prop({ required: true, type: SchemaTypes.String })
+  phoneNumber: string;
+  @Prop({ required: true, type: SchemaTypes.Date })
   birthday: Date;
 }
 
