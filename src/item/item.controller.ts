0bf4ab772f45ec48743b4ed0687122d2f5ba2db2
houssem.ammar.06@gmail.com <houssem.ammar.06@gmail.com>
2022-10-21 00:13:29 +0100

    implement bid system

--- src/item/item.controller.ts
+++ src/item/item.controller.ts
@@ -1,10 +1,18 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { StatusEnum } from 'src/types/item.types';
-import { CreateItemDto } from './item.dto';
+import { CreateItemDto, IdDto, PriceDto } from './item.dto';
 import { ItemService } from './item.service';
 import { ResponseObject } from '../abstract/response.object';
 import { Item } from './item.schema';
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
+import { Types } from 'mongoose';
 
 @Controller('item')
 export class ItemController {
@@ -23,5 +31,19 @@ export class ItemController {
       status: StatusEnum.drafted,
     });
     return new ResponseObject('ITEM_CREATED', data);
+  }
+
+  // bid on an item
+
+  @UseGuards(JwtAuthGuard)
+  @Post('/bid/:id')
+  async bidOnItem(
+    @Body() bid: PriceDto,
+    @Param() params: IdDto,
+  ): Promise<ResponseObject<Item>> {
+    console.log(params.id);
+    const user = new Types.ObjectId('635015011b4ccbb3b5a2c706');
+    const data = await this.itemService.bidOnItem(params.id, user, bid);
+    return new ResponseObject('ITEM_BID', data);
   }
 }
--- src/item/item.service.ts
+++ src/item/item.service.ts
@@ -1,10 +1,12 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { ForbiddenException, Inject, Injectable } from '@nestjs/common';
 import { AbstractService } from '../abstract/abstract.service';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
 import { Logger } from 'winston';
 import { Item } from './item.schema';
+import { PriceDto } from './item.dto';
+import { StatusEnum } from '../types/item.types';
 
 @Injectable()
 export class ItemService extends AbstractService<Item> {
@@ -15,4 +17,50 @@ export class ItemService extends AbstractService<Item> {
     super(itemModel, logger);
   }
   protected modelName = Item.name;
+
+  async bidOnItem(id: Types.ObjectId, user: Types.ObjectId, bid: PriceDto) {
+    const item = await this.findById(id, {
+      name: 1,
+      user: 1,
+      startedPrice: 1,
+      highestPrice: 1,
+      status: 1,
+      timeWindow: 1,
+      createdAt: 1,
+      updatedAt: 1,
+      bids: {
+        $filter: {
+          input: '$bids',
+          as: 'bids',
+          cond: {
+            $eq: ['$$bids.user', user],
+          },
+        },
+      },
+    });
+
+    if (item.status === StatusEnum.drafted) {
+      throw new ForbiddenException('You cannot bid item is not published');
+    }
+
+    if (new Date().getTime() >= item.timeWindow.getTime()) {
+      throw new ForbiddenException('Bid is over ');
+    }
+    // for now we only have one currency
+    if (item.highestPrice.amount >= bid.amount) {
+      throw new ForbiddenException('You must bid with a higher price');
+    }
+
+    const lastUserBid = item.bids.pop();
+    const timeBidDifference =
+      (new Date().getTime() - lastUserBid.createdAt.getTime()) / 1000;
+    if (timeBidDifference < 5) {
+      throw new ForbiddenException('Bid again after 5 seconds ');
+    }
+
+    return await this.findByIdAndUpdate(id, {
+      highestPrice: bid,
+      $push: { bids: { user, bidPrice: bid } },
+    });
+  }
 }
