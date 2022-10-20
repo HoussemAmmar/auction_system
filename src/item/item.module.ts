85fc19e2cffb41b97e1678cbc0f402dbd0bee2e0
HoussemAmmar <houssem.ammar.simplon@gmail.com>
2022-10-20 15:38:03 +0100

    implement bull queue

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -3,12 +3,17 @@ import { ItemController } from './item.controller';
 import { ItemService } from './item.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Item, ItemSchema } from './item.schema';
+import { BullModule } from '@nestjs/bull';
+import { ItemConsumer } from './item.consumer';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Item.name, schema: ItemSchema }]),
+    BullModule.registerQueue({
+      name: 'item-queue',
+    }),
   ],
   controllers: [ItemController],
-  providers: [ItemService],
+  providers: [ItemService, ItemConsumer],
 })
 export class ItemModule {}
